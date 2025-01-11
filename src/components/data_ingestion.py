698106a46e2b34f5e4dcee6fc7e1c2acf7f64d9a
Lakshaygoel4321 <iamlakshaygoel5990@gmail.com>
2025-01-11 10:47:45 +0530

    add utils

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -37,7 +37,6 @@ class DataIngestion:
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
-
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
